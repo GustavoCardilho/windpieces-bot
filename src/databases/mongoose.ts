7c7948d39acb07d0cbb4f5131649a859d2306824
Kyoudan <fogoste@gmail.com>
2023-09-05 11:28:43 -0300

    feat: adding mongodb in docker-compose

--- src/databases/mongoose.ts
+++ src/databases/mongoose.ts
@@ -5,7 +5,9 @@ const mongodbUrl = env.MONGODB_URL || env.MONGODB_URL_TEST;
 
 export async function startMongoDB() {
   try {
-    await mongoose.connect(mongodbUrl!);
+    await mongoose.connect(mongodbUrl!, {
+        
+    });
     console.log("Mongoose Connected".green);
   } catch (err) {
     console.log("Mongoose Error".red, err);
