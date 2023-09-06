fdbfc6a40957ba24d7e6fd073fa7feff1dc46996
Kyoudan <fogoste@gmail.com>
2023-09-06 10:51:41 -0300

    test: test lint

--- src/commands/common/guild.ts
+++ src/commands/common/guild.ts
@@ -3,40 +3,40 @@ import { Command } from "../../structs/types/command";
 import { GuildModel } from "../../models/GuildModel";
 
 export default new Command({
-name: "guild",
-description: "Registrar o servidor ao WindPieces",
-type: ApplicationCommandType.ChatInput,
-async run({ interaction, client, options }) {
-const guildId = interaction.guildId;
-const guildOwnerId = interaction.guild?.ownerId;
+  name: "guild",
+  description: "Registrar o servidor ao WindPieces",
+  type: ApplicationCommandType.ChatInput,
+  async run({ interaction, client, options }) {
+    const guildId = interaction.guildId;
+    const guildOwnerId = interaction.guild?.ownerId;
 
-if (!guildId || !guildOwnerId) {
-interaction.reply({
-ephemeral: true,
-content: `Dados insuficientes para registrar o servidor.`,
-});
-return;
-}
+    if (!guildId || !guildOwnerId) {
+      interaction.reply({
+        ephemeral: true,
+        content: `Dados insuficientes para registrar o servidor.`,
+      });
+      return;
+    }
 
-try {
-const checkGuild = await GuildModel.findOne({ guildID: guildId });
-if (checkGuild) {
-interaction.reply({
-ephemeral: true,
-content: `Este servidor já está registrado.`,
-});
-return;
-}
-const teste = await GuildModel.create({
-guildID: guildId,
-ownerID: guildOwnerId,
-});
-interaction.reply({
-ephemeral: true,
-content: `Servidor registrado com sucesso.`,
-});
-} catch (err) {
-console.log(err);
-}
-},
+    try {
+      const checkGuild = await GuildModel.findOne({ guildID: guildId });
+      if (checkGuild) {
+        interaction.reply({
+          ephemeral: true,
+          content: `Este servidor já está registrado.`,
+        });
+        return;
+      }
+      const teste = await GuildModel.create({
+        guildID: guildId,
+        ownerID: guildOwnerId,
+      });
+      interaction.reply({
+        ephemeral: true,
+        content: `Servidor registrado com sucesso.`,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  },
 });
