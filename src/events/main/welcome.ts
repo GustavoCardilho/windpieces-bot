310e9d648de2cb3ef54779afee5d80e89b8f6c20
Kyoudan <fogoste@gmail.com>
2023-09-05 23:31:28 -0300

    feat: adding embed in welcome event

--- src/events/main/welcome.ts
+++ src/events/main/welcome.ts
@@ -1,9 +1,26 @@
+import { EmbedBuilder } from "discord.js";
 import { client } from "../..";
 import { Event } from "../../structs/types/event";
 
 export default new Event({
   name: "guildMemberAdd",
   run(interaction) {
-    console.log("oi");
+    const embed = new EmbedBuilder()
+      .setTitle("Bem vindo ao servidor!")
+      .setDescription(
+        `Olá ${interaction.user.username}, seja bem vindo ao servidor!`,
+      )
+      .setColor("Green")
+      .setThumbnail(interaction.user.avatarURL()!);
+
+    const chanell: any = interaction.guild?.channels.cache.get(
+      "1102361851330826344",
+    );
+
+    if (chanell) {
+      chanell.send({
+        embeds: [embed],
+      });
+    }
   },
 });
